16ea79673022cc0dd00360cabaa487399fdcdd5a
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2024-12-25 10:25:20 +0200

    Updated some bad padding

--- lib/widgets/run_event_box.dart
+++ lib/widgets/run_event_box.dart
@@ -8,7 +8,7 @@ class RunEventBox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10),
+      padding: const EdgeInsets.only(left: 20),
       child: Container(
         color: AppColor.tileColor,
         width: AppMeasure.width / 1.2,
