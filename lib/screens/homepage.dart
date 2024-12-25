df998fa00ba3f55e978055a7af085707141b3b6e
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2024-12-25 10:14:26 +0200

    Created a training plan widget

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:runx_app/constants/app_constants.dart';
 import 'package:runx_app/widgets/run_event_box.dart';
+import 'package:runx_app/widgets/training_plan_box.dart';
 import 'package:runx_app/widgets/view_all_button.dart';
 
 import '../widgets/view_calendar_box.dart';
@@ -12,116 +13,112 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: AppColor.backgroundColor,
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(
-              height: 40,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Hi, Tinashe',
-                        style: TextStyle(
-                          color: AppColor.fontColor,
-                          fontWeight: FontWeight.bold,
+        body: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 40,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Hi, Tinashe',
+                          style: TextStyle(
+                            color: AppColor.fontColor,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                      ),
-                      Text(
-                        'Welcome Back!',
-                        style: TextStyle(
-                          color: AppColor.fontColor,
-                          fontWeight: FontWeight.w900,
-                          fontSize: 30,
+                        Text(
+                          'Welcome Back!',
+                          style: TextStyle(
+                            color: AppColor.fontColor,
+                            fontWeight: FontWeight.w900,
+                            fontSize: 30,
+                          ),
                         ),
+                      ],
+                    ),
+                    CircleAvatar(
+                      backgroundColor: AppColor.tileColor,
+                      child: Icon(
+                        Icons.notifications,
+                        color: AppColor.limeGreen,
                       ),
-                    ],
-                  ),
-                  CircleAvatar(
-                    backgroundColor: AppColor.tileColor,
-                    child: Icon(
-                      Icons.notifications,
-                      color: AppColor.limeGreen,
-                    ),
-                  )
-                ],
-              ),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            ViewCalendarBox(),
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Upcoming Runs',
-                    style: TextStyle(
-                      color: AppColor.fontColor,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 25,
-                    ),
-                  ),
-                  Text(
-                    'View All',
-                    style: TextStyle(
-                      fontSize: 12,
-                      color: AppColor.limeGreen,
-                      decoration: TextDecoration.underline,
-                    ),
-                  )
-                ],
-              ),
-            ),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: [
-                  RunEventBox(),
-                  RunEventBox(),
-                  RunEventBox(),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Text(
-                'Training Plans',
-                style: TextStyle(
-                  color: AppColor.fontColor,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 25,
+                    )
+                  ],
                 ),
               ),
-            ),
-            Row(
-              children: [
-                Container(
-                  child: Column(
-                    children: [
-                      Text('Beginner'),
-                      Column(
-                        children: [
-                          Text('5k'),
-                          ViewAllButton(
-                            buttonText: 'View Plans',
-                          )
-                        ],
-                      )
-                    ],
+              SizedBox(
+                height: 30,
+              ),
+              ViewCalendarBox(),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Upcoming Runs',
+                      style: TextStyle(
+                        color: AppColor.fontColor,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25,
+                      ),
+                    ),
+                    Text(
+                      'View All',
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: AppColor.limeGreen,
+                        decoration: TextDecoration.underline,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    RunEventBox(),
+                    RunEventBox(),
+                    RunEventBox(),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Text(
+                  'Training Plans',
+                  style: TextStyle(
+                    color: AppColor.fontColor,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25,
                   ),
-                )
-              ],
-            )
-          ],
+                ),
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    TrainingPlanBox(distanceLength: '5K'),
+                    TrainingPlanBox(distanceLength: '10K'),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 1000,
+              )
+            ],
+          ),
         ));
   }
 }
---
+++ lib/widgets/training_plan_box.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+import '../constants/app_constants.dart';
+import 'view_all_button.dart';
+
+class TrainingPlanBox extends StatelessWidget {
+  final String distanceLength;
+  const TrainingPlanBox({super.key, required this.distanceLength});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 20),
+      child: Row(
+        children: [
+          Container(
+            width: AppMeasure.width / 2,
+            height: AppMeasure.height / 4,
+            color: AppColor.tileColor,
+            padding: EdgeInsets.all(15),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Beginner',
+                  style: TextStyle(
+                    color: AppColor.fontColor,
+                    fontSize: 10,
+                  ),
+                ),
+                Center(
+                  child: Column(
+                    children: [
+                      Text(
+                        distanceLength,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 90,
+                          color: AppColor.fontColor,
+                        ),
+                      ),
+                      ViewAllButton(
+                        buttonText: 'View Plan',
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
