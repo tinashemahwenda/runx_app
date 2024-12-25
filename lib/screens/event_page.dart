07b154ce38e7133e8616bb06df406f09c03e2460
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2024-12-25 10:31:23 +0200

    Created a page to view a race event

---
+++ lib/screens/event_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import '../constants/app_constants.dart';
+
+class EventPage extends StatelessWidget {
+  const EventPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColor.backgroundColor,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [],
+      ),
+    );
+  }
+}
