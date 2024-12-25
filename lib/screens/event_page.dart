c29a19f1ce13bd31b70c0358c882d8f6a39dfd8d
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2024-12-25 10:40:44 +0200

    Created a navbar for the events page

--- lib/screens/event_page.dart
+++ lib/screens/event_page.dart
@@ -11,7 +11,37 @@ class EventPage extends StatelessWidget {
       backgroundColor: AppColor.backgroundColor,
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: [],
+        children: [
+          SizedBox(
+            height: 60,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'All Runs 2025',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: AppColor.fontColor,
+                    fontSize: 25,
+                  ),
+                ),
+                CircleAvatar(
+                  backgroundColor: AppColor.tileColor,
+                  child: Icon(
+                    Icons.bookmark,
+                    color: AppColor.limeGreen,
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+        ],
       ),
     );
   }
