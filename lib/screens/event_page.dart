42960f69d9b81a9091f0132d45d5e4a9b7fe7648
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2024-12-29 21:51:22 +0200

    Updated events page

--- lib/screens/event_page.dart
+++ lib/screens/event_page.dart
@@ -12,69 +12,85 @@ class EventPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColor.backgroundColor,
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: 60,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  GestureDetector(
-                    onTap: () => Navigator.pop(context),
-                    child: Row(
-                      children: [
-                        Icon(
-                          Icons.arrow_back_ios_new,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 60,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                GestureDetector(
+                  onTap: () => Navigator.pop(context),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.arrow_back_ios_new,
+                        color: AppColor.fontColor,
+                        size: 20,
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        'All Runs 2025',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
                           color: AppColor.fontColor,
-                          size: 20,
+                          fontSize: 25,
                         ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Text(
-                          'All Runs 2025',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: AppColor.fontColor,
-                            fontSize: 25,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  CircleAvatar(
-                    backgroundColor: AppColor.tileColor,
-                    child: Icon(
-                      Icons.bookmark,
-                      color: AppColor.limeGreen,
-                    ),
-                  )
-                ],
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              /* Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Text(
-                  'January 2025',
-                  style: TextStyle(
-                    color: AppColor.fontColor,
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
+                      ),
+                    ],
                   ),
                 ),
-              ),*/
-              SizedBox(
-                height: 20,
+                CircleAvatar(
+                  backgroundColor: AppColor.tileColor,
+                  child: Icon(
+                    Icons.bookmark,
+                    color: AppColor.limeGreen,
+                  ),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            /* Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Text(
+                'January 2025',
+                style: TextStyle(
+                  color: AppColor.fontColor,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ],
-          ),
+            ),*/
+            Expanded(
+                child: SizedBox(
+              height: AppMeasure.height,
+              child: ListView.builder(
+                itemCount: event.length,
+                itemBuilder: (context, index) {
+                  if (index < event.length) {
+                    final race = event[index];
+                    return RunEventBox(
+                      raceTitle: race.event,
+                      raceLocation: race.venue,
+                      raceDate: race.date,
+                    );
+                  } else {
+                    return SizedBox(
+                      height: AppMeasure.height,
+                    );
+                  }
+                },
+              ),
+            ))
+          ],
         ),
       ),
     );
