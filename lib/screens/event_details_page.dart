0c5c6259cf28ac04dcb309ca7501ee734c8ed75b
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2024-12-31 04:41:06 +0200

    Created a nice event details card and heading

--- lib/screens/event_details_page.dart
+++ lib/screens/event_details_page.dart
@@ -11,13 +11,52 @@ class EventDetailsPage extends StatelessWidget {
       backgroundColor: AppColor.backgroundColor,
       body: Column(
         children: [
-          SizedBox(
-            width: AppMeasure.width,
-            height: AppMeasure.height / 2,
-            child: Image.asset(
-              'assets/images/event-bg.png',
-              fit: BoxFit.cover,
-            ),
+          Stack(
+            children: [
+              SizedBox(
+                width: AppMeasure.width,
+                height: AppMeasure.height / 2,
+                child: Image.asset(
+                  'assets/images/event-bg.png',
+                  fit: BoxFit.fitHeight,
+                ),
+              ),
+              Container(
+                width: AppMeasure.width,
+                height: AppMeasure.height,
+                color: Colors.black54,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Container(
+                  width: AppMeasure.width,
+                  height: AppMeasure.height / 2,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: AppMeasure.height / 2.8,
+                      ),
+                      Text(
+                        'Piers Road Run',
+                        style: TextStyle(
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
+                          color: AppColor.limeGreen,
+                        ),
+                      ),
+                      Text(
+                        'Gerfunkels Grill | 05/01/2025',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: AppColor.fontColor,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
           )
         ],
       ),
