b8d727fc30166402dbd6d6c46347a311eef347ca
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2024-12-31 04:51:38 +0200

    Done with the basic details page layout

--- lib/screens/event_details_page.dart
+++ lib/screens/event_details_page.dart
@@ -9,56 +9,86 @@ class EventDetailsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColor.backgroundColor,
-      body: Column(
-        children: [
-          Stack(
-            children: [
-              SizedBox(
-                width: AppMeasure.width,
-                height: AppMeasure.height / 2,
-                child: Image.asset(
-                  'assets/images/event-bg.png',
-                  fit: BoxFit.fitHeight,
-                ),
-              ),
-              Container(
-                width: AppMeasure.width,
-                height: AppMeasure.height,
-                color: Colors.black54,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Container(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                SizedBox(
                   width: AppMeasure.width,
                   height: AppMeasure.height / 2,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(
-                        height: AppMeasure.height / 2.8,
-                      ),
-                      Text(
-                        'Piers Road Run',
-                        style: TextStyle(
-                          fontSize: 40,
-                          fontWeight: FontWeight.bold,
-                          color: AppColor.limeGreen,
-                        ),
-                      ),
-                      Text(
-                        'Gerfunkels Grill | 05/01/2025',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: AppColor.fontColor,
-                        ),
-                      ),
-                    ],
+                  child: Image.asset(
+                    'assets/images/event-bg.png',
+                    fit: BoxFit.fitHeight,
                   ),
                 ),
-              )
-            ],
-          )
-        ],
+                Container(
+                  width: AppMeasure.width,
+                  height: AppMeasure.height / 2,
+                  color: Colors.black54,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Container(
+                    width: AppMeasure.width,
+                    height: AppMeasure.height / 2,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          height: AppMeasure.height / 2.8,
+                        ),
+                        Text(
+                          'Piers Road Run',
+                          style: TextStyle(
+                            fontSize: 40,
+                            fontWeight: FontWeight.bold,
+                            color: AppColor.limeGreen,
+                          ),
+                        ),
+                        Text(
+                          'Gerfunkels Grill | 05/01/2025',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: AppColor.fontColor,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text(
+                "Whether you're an experienced runner or just getting started, this event offers a fun and supportive environment for all fitness levels. With scenic routes, enthusiastic participants, and a chance to challenge yourself, it's the perfect opportunity to get moving, set personal goals, and enjoy the great outdoors. Participants will receive a race medal and a goody bag, with prizes for top finishers in various categories",
+                style: TextStyle(
+                  color: AppColor.fontColor,
+                  height: 2,
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(40),
+              child: Container(
+                width: AppMeasure.width / 2,
+                padding: EdgeInsets.all(20),
+                color: AppColor.limeGreen,
+                child: Center(
+                  child: Text(
+                    'Register',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
