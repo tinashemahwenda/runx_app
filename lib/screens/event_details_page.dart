79918ba00a340914bfcabd2ee9d127cb9bfa1e60
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-01-06 10:04:15 +0200

    Made a cool looking time bubble

--- lib/screens/event_details_page.dart
+++ lib/screens/event_details_page.dart
@@ -84,13 +84,32 @@ class EventDetailsPage extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Container(
-                    padding: EdgeInsets.all(5),
-                    child: Text(
-                      'Time: 0800hrs',
-                      style: TextStyle(
-                        color: AppColor.fontColor,
-                        fontSize: 10,
-                      ),
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: AppColor.backgroundColor,
+                        border: Border.all(
+                          width: 1,
+                          color: AppColor.tileColor,
+                        )),
+                    child: Column(
+                      children: [
+                        Icon(
+                          Icons.watch,
+                          color: AppColor.limeGreen,
+                        ),
+                        Container(
+                          padding: EdgeInsets.all(5),
+                          child: Text(
+                            '0800hrs',
+                            style: TextStyle(
+                              color: AppColor.fontColor,
+                              fontSize: 12,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                   Text(
